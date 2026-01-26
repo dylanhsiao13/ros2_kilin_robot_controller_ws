b80a21bbdee99517b1c4f5bc357124e6ca1421c0
dylanhsiao13 <dylanhsiao13@gmail.com>
2026-01-26 12:17:06 +0800

    modify setup.py to include ['launch/spawn_kilin.launch.py']

--- src/kilin_gazebo/setup.py
+++ src/kilin_gazebo/setup.py
@@ -8,8 +8,10 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/launch',
+        ['launch/spawn_kilin.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
