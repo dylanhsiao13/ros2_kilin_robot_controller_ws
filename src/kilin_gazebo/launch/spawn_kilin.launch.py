fe0e9b73bc070ab324c29133e8ea871cfe35825c
dylanhsiao13 <dylanhsiao13@gmail.com>
2026-01-26 11:29:01 +0800

    add spawn_kilin.launch.py

---
+++ src/kilin_gazebo/launch/spawn_kilin.launch.py
@@ -0,0 +1,54 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
+def generate_launch_description():
+
+    # Path to URDF
+    robot_description_path = PathJoinSubstitution([
+        FindPackageShare("kilin_description"),
+        "urdf",
+        "URDF.urdf"
+    ])
+
+    # Gazebo
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare("gazebo_ros"),
+                "launch",
+                "gazebo.launch.py"
+            ])
+        ])
+    )
+
+    # robot_state_publisher
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{
+            "robot_description": Command(["xacro ", robot_description_path])
+        }],
+        output="screen"
+    )
+
+    # Spawn entity
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=[
+            "-topic", "robot_description",
+            "-entity", "kilin"
+        ],
+        output="screen"
+    )
+
+    return LaunchDescription([
+        gazebo,
+        robot_state_publisher,
+        spawn_entity
+    ])
